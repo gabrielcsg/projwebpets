286671a1f3e79be0d3fa304a2f768953b97f6c29
Edvaldo <edvaldoh.leite@gmail.com>
2021-01-15 21:20:42 -0300

    Relacionamento endereço e interessado

---
+++ database/migrations/2021_01_15_131754_create_enderecos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnderecosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enderecos', function (Blueprint $table) {
+            $table->id();
+            $table->cidade();
+            $table->bairro();
+            $table->estado();
+            $table->cep();
+            $table->logradouro();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enderecos');
+    }
+}
