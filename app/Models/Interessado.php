0d41ac267d5f05548fc6dc1e027448c2508aff57
Gabriel <gabrielcssg@gmail.com>
2021-01-27 22:26:27 -0300

    rules e validators interessado

--- app/Models/Interessado.php
+++ app/Models/Interessado.php
@@ -8,6 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Interessado extends Model
 {
     use HasFactory;
+
+    public static $rules = [
+        'nome' => 'required|min:3|max:100',
+        'data_nascimento' => 'required',
+        'telefone' => 'required',
+    ];
+
+    public static $messages = [
+        'nome.*' => 'O nome é obrigatório e deve ter entre 3 à 100 caracteres',
+        'data_nascimento.required' => 'A data de nascimento é obrigatória',
+        'telefone.required' => 'O telefone é obrigatório',
+    ];
     
     public function endereco(){
     	return $this->belongsTo("\App\Models\Endereco");
