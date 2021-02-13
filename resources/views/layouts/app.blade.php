a1eb03f68982c6013616304af877c29f034c3334
Gabriel Castro <gabrielcssg@gmail.com>
2021-02-13 17:03:23 -0300

    link para home no navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,6 +40,9 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a>
+                        </li>
                         <!-- Authentication Links -->
                         @guest
                         @if (Route::has('login'))
