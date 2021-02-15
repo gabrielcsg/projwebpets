376921df9d85ab2833ff32a1dffbda538402b34d
Gabriel Castro <gabrielcssg@gmail.com>
2021-02-15 20:26:03 -0300

    removido campo name de user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -31,7 +31,6 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'tipo' => ['required'],
@@ -42,7 +41,6 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         $user = User::create([
-            'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
             'tipo' => $data['tipo']
@@ -62,7 +60,7 @@ class RegisterController extends Controller
 
         if ($user->tipo == 'interessado') {
             $data_interessado = [
-                'nome' => $data['name'],
+                'nome' => $data['nome'],
                 'data_nascimento' => $data['data_nascimento'],
                 'telefone' => $data['telefone'],
                 'user_id' => $user->id,
--- resources/views/formInteressado.blade.php
+++ resources/views/formInteressado.blade.php
@@ -4,6 +4,20 @@
 <input id="tipo" type="hidden" name="tipo" value="interessado">
 
 <div class="form-group row">
+    <label for="nome" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
+
+    <div class="col-md-6">
+        <input id="nome" type="text" class="form-control @error('nome') is-invalid @enderror" name="nome" value="{{ old('nome') }}" required autocomplete="nome" autofocus>
+
+        @error('nome')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+    </div>
+</div>
+
+<div class="form-group row">
     <label for="data_nascimento" class="col-md-4 col-form-label text-md-right">{{ __('Data de nascimento') }}</label>
 
     <div class="col-md-6">
