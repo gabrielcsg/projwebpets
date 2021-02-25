9e552bf5633199dea151980a3f5e88a35fda4b22
EdvaldoHLeite <edvaldoh.leite@gmail.com>
2021-02-25 14:36:22 -0300

    Ajustando view da listagem de ongs para padrão

--- resources/views/ongs/listOngs.blade.php
+++ resources/views/ongs/listOngs.blade.php
@@ -1,21 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ongs</title>
-</head>
+@extends('layouts.app')
 
-<body>
-    <h1 align="center">Ongs Cadastradas</h1>
-    <a href="/ongs/cadastro">Cadastrar nova ONG</a>
-    <ul>
-        @foreach ($ongs as $ong)
-        <li>#{{ $ong->id }} - {{ $ong->nome_fantasia }}
-        </li>
-        @endforeach
-    </ul>
-</body>
+@section('content')
+    <body>
+        <h1 align="center">Ongs Cadastradas</h1>
+        <a href="/ongs/cadastro">Cadastrar nova ONG</a>
+        <ul align="center">
+            @foreach ($ongs as $ong)
+            <li>{{ $ong->nome_fantasia }}
+            </li>
+            @endforeach
+        </ul>
+    </body>
 
-</html>
+    </html>
+@endsection
